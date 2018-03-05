3c7458897ac3331af59caecca35f4e4846bef6a7
Artem Shibaev <2fasti.miem@gmail.com>
2018-03-05 14:39:56 +0300

    added more stations. Improved interface.

--- server.py
+++ server.py
@@ -1,29 +1,27 @@
-import subprocess
-import time
+import json
 from threading import Thread
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from player import mpg123Player, mpg123PlayerError
+
 
 class ServerError(Exception):
     pass
 
+
 class Server(object):
     """ class to control mpg123 player """
-    def __init__(self, url=None):
-        if url is None:
+    def __init__(self, station=None):
+        if station is None:
             self.url = 'http://nashe2.hostingradio.ru/ultra-128.mp3'
         else:
-            self.url = url
-        self.p = None
-        self.isStoped = False
+            self.url = json.load(open('urls', 'r'))[station]
+        self.player = None
+        self.isEnabled = False
         self.history = []
+        self.isRunning = False
+        self.current_radio_station = station
 
-    def read_info(self):
-        """ read meta-info """
-        for line in iter(self.p.stdout.readline, b''):
-            # парсер сначала
-            print(line.decode())
 
-    def add_history(self, track):
+    def store_history(self, track):
         """
         store tracks history
 
@@ -31,20 +29,32 @@ class Server(object):
 
         :return:
         """
-        if len(self.history) > 5:
+        if len(self.history) > 4:
             self.history.pop(0)
+            self.history.append(self.player.get_current_track)
         else:
-            self.history.append(track)
+            self.history.append(self.player.get_current_track)
+
+    @property
+    def get_history(self):
+        return self.history
+
+    @property
+    def get_current_radio_station(self):
+        return self.current_radio_station
 
     def run(self):
         while True:
-            if not self.isStoped:
-                self.p = subprocess.Popen(['mpg123', '-@', self.url], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-                out, err = self.p.communicate()
-                time.sleep(5)
+            if not self.isEnabled and not self.isRunning:
+                self.player = mpg123Player(self.history)
+                self.isRunning = True
+                self.player.run()
+                self.player.play(self.url)
+                self.history = []
             else:
-                print('Stop command received')
-                break
+                for track in self.player._read:
+                    self.store_history(track)
+                    continue
 
     @property
     def stop(self):
@@ -54,11 +64,11 @@ class Server(object):
         :return: True or False
 
         """
-        if self.p is not None:
+        if self.player is not None:
             try:
                 print('Stop command send')
-                self.isStoped = True
-                self.p.kill()
+                self.isEnabled = True
+                self.player.stop()
             except Exception as err:
                 raise ServerError('Cant stop radio server. {error}'.format(error=err))
             else:
@@ -70,12 +80,9 @@ class Server(object):
     def start(self):
         th1 = Thread(target=self.run, args=())
         th1.start()
-        # with ThreadPoolExecutor(max_workers=2) as pool:
-        #     futures = [pool.submit(self.run(),)
+        #self.run()
 
 if __name__ == '__main__':
     x = Server(url='http://nashe2.hostingradio.ru/ultra-128.mp3')
 
     x.start
-    time.sleep(25)
-    x.stop
